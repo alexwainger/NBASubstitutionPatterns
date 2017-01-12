60fcce08903e49a8f723527414f178dc6fccfc8c
Alex Wainger <alex_wainger@brown.edu>
2017-01-11 22:09:39 -0700

    Weekly update

--- code/python/scraper.py
+++ code/python/scraper.py
@@ -132,7 +132,7 @@ def process_plus_minus(plus_minus_link, isHomeGame, num_overtimes, players):
 def main():
 
 	today = datetime.now().date();
-	years = [];
+	years = ["2017"];
 
 	for year in years:
 		print "DOING YEAR " + year;
